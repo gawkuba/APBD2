adb62d3ce52ffd145f4f67499ee06bc35cda4a12
gawelczak <gawkuba@gmail.com>
2026-03-28 22:27:20 +0100

    Praca nad klasami Camera Laptop i Projector

--- UniversityEquipmentRental/Domain/Equipment/Laptop.cs
+++ UniversityEquipmentRental/Domain/Equipment/Laptop.cs
@@ -1,6 +1,18 @@
 ﻿namespace UniversityEquipmentRental.Domain.Equipment;
 
-public class Laptop
+public class Laptop : Equipment
 {
-    
+    public int RamGb { get; }
+    public string Processor { get; }
+
+    public Laptop(string name, int ramGb, string processor) : base(name)
+    {
+        RamGb = ramGb;
+        Processor = processor;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $", RAM: {RamGb}GB, CPU: {Processor}";
+    }
 }
