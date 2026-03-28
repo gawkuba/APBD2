fca7261c93d4b0aac98e4332e1458825c80cbdfe
gawelczak <gawkuba@gmail.com>
2026-03-28 23:04:07 +0100

    Praca nad repozytoriami w drugim branchu

--- UniversityEquipmentRental/Repositories/IEquipmentRepository.cs
+++ UniversityEquipmentRental/Repositories/IEquipmentRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace UniversityEquipmentRental.Repositories;
+﻿using UniversityEquipmentRental.Domain.Equipment;
+
+namespace UniversityEquipmentRental.Repositories;
 
 public interface IEquipmentRepository
 {
-    
+    void Add(Equipment equipment);
+    Equipment? GetById(int id);
+    IEnumerable<Equipment> GetAll();
 }
--- UniversityEquipmentRental/Repositories/IUserRepository.cs
+++ UniversityEquipmentRental/Repositories/IUserRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace UniversityEquipmentRental.Repositories;
+﻿using UniversityEquipmentRental.Domain.Users;
+
+namespace UniversityEquipmentRental.Repositories;
 
 public interface IUserRepository
 {
-    
+    void Add(User user);
+    User? GetById(int id);
+    IEnumerable<User> GetAll();
 }
