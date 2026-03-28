b7360fe4e335b9f0e73610ff00068b1ed53f2708
gawelczak <gawkuba@gmail.com>
2026-03-28 23:44:32 +0100

    polityki i serwisy

--- UniversityEquipmentRental/Services/UserPolicy.cs
+++ UniversityEquipmentRental/Services/UserPolicy.cs
@@ -1,6 +1,16 @@
-﻿namespace UniversityEquipmentRental.Services;
+﻿using UniversityEquipmentRental.Domain.Users;
+
+namespace UniversityEquipmentRental.Services;
 
 public class UserPolicy
 {
-    
+    public int GetMaxActiveRentals(User user)
+    {
+        return user switch
+        {
+            Student => 2,
+            Employee => 5,
+            _ => throw new InvalidOperationException("Nieznany typ użytkownika.")
+        };
+    }
 }
